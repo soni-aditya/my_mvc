f8ce8b6dbc4a25d1a6c8710903d803bca71318a1
Aditya Soni <adityasoni5859@gmail.com>
2016-09-18 12:19:52 +0530

    applied routing via functions instead of switch case

--- index.php
+++ index.php
@@ -5,23 +5,30 @@
  * website-www.techieeasy.com
  */
 if(isset($_GET['url'])){
-    echo "My First MVC TEsT<br>";
     $url=$_GET['url'];
     $url=rtrim($url,"/");
     $url=explode("/",$url);
-    //var_dump($url);
+    /*Here we are applying functions instead of switch case
+       *its the more recommended method
+    */
+    function about($name){
+        echo "This page is about ".$name;
+    }
+    function services($service){
+        echo "This page is about ".$service;
+    }
+    function contact($number){
+        echo "This page is about ".$number;
+    }
 
-    switch ($url['0']){
-        case 'about'    :
-            echo "This is about Page";
-            break;
-        case 'services' :
-            echo "This is about Services";
-            break;
-        case 'contact'  :
-            echo "This is about Contacts";
-            break;
-    }//sdf
+    if(function_exists($url['0'])){
+        if(isset($url['1'])){
+            $url['0']($url['1']);
+        }
+        else{
+            $url['0']('a');
+        }
+    }
 }
 else{
     echo "My First MVC TEsT";
