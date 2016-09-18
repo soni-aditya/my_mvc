269183892a17e9ebc0ad0c5d4b1ad9980a4ab198
Aditya Soni <adityasoni5859@gmail.com>
2016-09-18 19:08:29 +0530

    Applying Object Routing [with only one controller class]

---
+++ app/controller.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adi
+ * Date: 9/18/2016
+ * Time: 2:32 PM
+ */
+//package= app/controller.php
+class AppController{
+    function main(){
+        echo "this is from main";
+    }
+    function about(){
+        echo "this is from about";
+    }
+    function contact(){
+        echo "this is from about";
+    }
+    function services(){
+        echo "these are the services";
+    }
+}
--- index.php
+++ index.php
@@ -4,32 +4,35 @@
  * AUTHOR-Aditya Soni
  * website-www.techieeasy.com
  */
-if(isset($_GET['url'])){
-    $url=$_GET['url'];
-    $url=rtrim($url,"/");
-    $url=explode("/",$url);
-    /*Here we are applying functions instead of switch case
-       *its the more recommended method
-    */
-    function about($name){
-        echo "This page is about ".$name;
-    }
-    function services($service){
-        echo "This page is about ".$service;
-    }
-    function contact($number){
-        echo "This page is about ".$number;
-    }
+//OBJECT ROUTING
 
-    if(function_exists($url['0'])){
-        if(isset($url['1'])){
-            $url['0']($url['1']);
-        }
-        else{
-            $url['0']('a');
+class My_Mvc{
+    public $routes=array();
+
+    function router(){
+        if(isset($_GET['url']))
+        {
+            $url=$_GET['url'];
+            $url=rtrim($url,'/');
+            $url=explode("/",$url);
+
+            foreach ($url as $key=>$value){
+                $this->routes[$key]=$value;
+            }
+            require_once("app/controller.php");
+            if(isset($this->routes['0'])){
+                $method=$this->routes['0'];
+                if(method_exists('AppController',$method)){
+                    $controller=new AppController();
+                    $controller->$method();
+                }
+                else{
+                    $controller=new AppController();
+                    $controller->main();
+                }
+            }
         }
     }
 }
-else{
-    echo "My First MVC TEsT";
-}
+$newObject=new My_Mvc();
+$newObject->router();
